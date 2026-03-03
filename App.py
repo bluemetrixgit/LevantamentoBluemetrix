86a7e39936f0a9b088958d038689e4d66442b7c2
bluemetrixgit <165940023+bluemetrixgit@users.noreply.github.com>
2026-03-03 14:25:42 -0300

    Update App.py

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ from datetime import datetime
 
 # ====================== CONFIGURAÇÕES FÁCEIS DE ALTERAR ======================
 # URL do arquivo Excel (link RAW do GitHub)
-GITHUB_RAW_URL = "https://github.com/bluemetrixgit/Levantamento-Bluemetrix/blob/main/Controle%20de%20Contratos%20-%20Atualizado%202026.xlsx"
+GITHUB_RAW_URL = "GITHUB_RAW_URL = "https://raw.githubusercontent.com/bluemetrixgit/Levantamento-Bluemetrix/main/Controle%20de%20Contratos%20-%20Atualizado%202026.xlsx""
 
 # URL da logo (link RAW do GitHub - use a versão branca como você indicou)
 LOGO_URL = "https://raw.githubusercontent.com/bluemetrixgit/Levantamento-Bluemetrix/main/logo.branca.png"
@@ -245,3 +245,4 @@ st.caption(
     """
 
 )
+
